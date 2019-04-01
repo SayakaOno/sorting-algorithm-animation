6484937810af568d17d3291c16321bf337e6f865
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-03-31 17:00:49 -0700

    implement setInterval to selection sort

--- src/Algorithms/selectionSort.js
+++ src/Algorithms/selectionSort.js
@@ -1,16 +1,59 @@
-function selectionSort(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    let temp = array[i];
-    let minIndex = i;
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[minIndex] > array[j]) {
-        minIndex = j;
+function selectionSort(
+  arra,
+  i1,
+  j1,
+  minIndex,
+  setSelectionMinIndex,
+  setSelectionArray,
+  setSelectionCounter,
+  ms
+) {
+  let fnc = function() {
+    let array = arra.slice();
+    let length = array.length;
+    let i = i1;
+    let j = j1;
+
+    return function() {
+      if (i < length - 1) {
+        if (j < length) {
+          j++;
+        }
+        if (j === length) {
+          if (i === length - 2) {
+            clearInterval(id);
+            return;
+          }
+          i++;
+          j = i + 1;
+        }
+        setSelectionCounter({ i, j });
       }
-    }
-    array[i] = array[minIndex];
-    array[minIndex] = temp;
-  }
-  return array;
+
+      if (i < array.length - 1) {
+        if (j < array.length) {
+          if (array[minIndex] > array[j]) {
+            minIndex = j;
+            setSelectionMinIndex(minIndex);
+          }
+        }
+        if (j === array.length - 1) {
+          array = swap(array, i, minIndex, setSelectionArray);
+          minIndex = i + 1;
+        }
+      }
+    };
+  };
+  let id = setInterval(fnc(), ms);
+}
+
+function swap(array, i, j, setSelectionArray) {
+  let newArray = array.slice();
+  let temp = newArray[i];
+  newArray[i] = newArray[j];
+  newArray[j] = temp;
+  setSelectionArray(newArray);
+  return newArray;
 }
 
 export default selectionSort;
